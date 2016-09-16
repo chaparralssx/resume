fcbf492b0192fe0bb138e09baa3af33386dd7536
R. Scott <rscott@theblameys.net>
2016-09-15 23:41:41 -0500

    Populate with experience

--- src/app/client/default/services/experienceData.service.js
+++ src/app/client/default/services/experienceData.service.js
@@ -12,7 +12,23 @@ function experienceData() {
             state: 'WI',
             start: 'July 2015',
             end: 'Present',
-            description: 'TECHNOLOGIES: HTML/CSS/JS, Angular, Node/NPM, Bower, Continuous Integration, Protractor, GIT, Photoshop, Visio, OpenShift<br>• Constructed Optum HSA Online Enrollment replacement portal user interface<br>• Constructing Optum Financial Services replacement portal user interfaces<br>• Developed Bootstrap-LESS-based multi-brand capable CSS framework with strict adherence to brand guidelines<br>• Automate end-to-end tests with Protractor<br>• Created, populated and manage Optum Financial Services Portals team wiki<br>• Implemented node-kss methodology for documenting CSS and generating style guides, which improves dev. consistency<br>• Presented methodologies to Common UX Councils as subject matter expert'
+            description: 'TECHNOLOGIES: HTML/CSS/JS, Angular, Node/NPM, Bower, Continuous Integration, Protractor, GIT, Photoshop, Visio, OpenShift<br>• Constructed Optum HSA Online Enrollment replacement portal user interface<br>• Constructing Optum Financial Services replacement portal user interfaces<br>• Developed Bootstrap-LESS-based multi-brand capable CSS framework with strict adherence to brand guidelines<br>• Automate end-to-end tests with Protractor<br>• Created, populated and manage Optum Financial Services Portals team wiki<br>• Implemented node-kss methodology for documenting CSS and generating style guides, which improves dev. consistency<br>• Presented methodologies to Common UX Councils as subject matter expert',
+            tech: [
+                'HTML/CSS/JS',
+                'Angular',
+                'Node',
+                'Bower',
+                'Hapi',
+                'Docker',
+                'Gulp',
+                'OpenShift',
+                'Jenkins',
+                'Protractor',
+                'SVN/GIT',
+                'Visio',
+                'Photoshop',
+                'Rally'
+            ]
         },
         {
             position: 'Business Systems Architect and Development Director',
@@ -22,67 +38,300 @@ function experienceData() {
             state: 'WI',
             start: 'May 2012',
             end: 'July 2015',
-            description: 'General Responsibilities<br>•	Client relationships<br>•	Requirements gathering<br>•	Proposal writing<br>•	Project management<br>•	Application architecture<br>•	Full stack web development	<br>•	Manage and mentor 6 developer team<br>•	Process improvement <br>•	Recruit and hire<br>•	Organize training<br>•	Community outreach<br><br>Accomplishments<br>•	Introduced Agile development methodology to the organization; implemented Target Process and Trello project boards <br>•	Documented workflows with Visio to standardize processes and identify opportunities for automation<br>•	Created a wiki to centralize operating processes and resources<br>•	Engineered and constructed MARKEY Digital Signage platform used in banks, restaurants, dealerships and other businesses<br><br>Select Projects<br>•	Continuing Education registration and management portal for 13 UW Colleges<br>•	Employee training and testing application for WI County Mutual Insurance, TTI, and Condon Oil companies<br>•	ClientWhys.com tax accountant CRM and turnkey website portal for ClientWhys (900+ customers)<br>•	TaxBuzz.com national tax accounting listing website (over 470,000 listings) for ClientWhys<br>•	Forms Manager (infoproforms.com) subscription-based PDF-generating and delivery web application for Info-Pro Information Management Solutions (over 440 accounts, 2400 subscriptions)<br>•	Inventory check-in application for Holiday Automotive'
+            description: 'General Responsibilities<br>•	Client relationships<br>•	Requirements gathering<br>•	Proposal writing<br>•	Project management<br>•	Application architecture<br>•	Full stack web development	<br>•	Manage and mentor 6 developer team<br>•	Process improvement <br>•	Recruit and hire<br>•	Organize training<br>•	Community outreach<br><br>Accomplishments<br>•	Introduced Agile development methodology to the organization; implemented Target Process and Trello project boards <br>•	Documented workflows with Visio to standardize processes and identify opportunities for automation<br>•	Created a wiki to centralize operating processes and resources<br>•	Engineered and constructed MARKEY Digital Signage platform used in banks, restaurants, dealerships and other businesses<br><br>Select Projects<br>•	Continuing Education registration and management portal for 13 UW Colleges<br>•	Employee training and testing application for WI County Mutual Insurance, TTI, and Condon Oil companies<br>•	ClientWhys.com tax accountant CRM and turnkey website portal for ClientWhys (900+ customers)<br>•	TaxBuzz.com national tax accounting listing website (over 470,000 listings) for ClientWhys<br>•	Forms Manager (infoproforms.com) subscription-based PDF-generating and delivery web application for Info-Pro Information Management Solutions (over 440 accounts, 2400 subscriptions)<br>•	Inventory check-in application for Holiday Automotive',
+            tech: [
+                'HTML/CSS/JS',
+                'PHP',
+                'MySQL',
+                'BrowserStack',
+                'SVN',
+                'Visio',
+                'Balsamiq',
+                'Photoshop',
+                'Trello',
+                'Target Process'
+            ]
         },
         {
-            position: '',
-            company: '',
+            position: 'Owner',
+            company: 'Ledgeview Associates, LLC',
             companyDesc: '',
-            city: '',
-            state: '',
-            start: '',
-            end: '',
-            description: ''
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: 'September 2012',
+            end: 'Present',
+            description: 'Lessor, Property Manager'
         },
         {
-            position: '',
-            company: '',
-            companyDesc: '',
-            city: '',
-            state: '',
-            start: '',
-            end: '',
-            description: ''
+            position: 'Owner',
+            company: 'Chaparral Enterprises, LLC dba R-Tech / Self Employed',
+            companyDesc: 'IT support and consulting',
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: 'January 2002',
+            end: 'Present',
+            description: '• Provide business IT infrastructure design and support services<br>• Installed and configured automotive dealership sales lead systems which increased Internet-sourced sales more than tenfold<br>• Developed a backup and imaging process that restored computers to better-than-new condition in less than one day',
+            tech: [
+                'HTML/CSS/JS',
+                'PHP',
+                'MySQL',
+                'SVN',
+                'MVC',
+                'Windows OSs'
+            ]
         },
         {
-            position: '',
-            company: '',
-            companyDesc: '',
-            city: '',
-            state: '',
-            start: '',
-            end: '',
-            description: ''
+            position: 'Founder',
+            company: 'Emergent Technology Center',
+            companyDesc: 'Seed accelerator, training center and collaborative working space',
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: 'April 2014',
+            end: 'January 2015',
+            description: '<p>A seed accelerator, emerging technology training center and collaborative working space in Fond du Lac to launch new businesses and develop advanced tech talent northeastern Wisconsin.</p><p>The concept was presented to community leaders in early April and is now supported by the majority of those in initial attendance as well as several other key organizations in the northeastern Wisconsin.</p>',
+            tech: [
+
+            ]
         },
         {
-            position: '',
-            company: '',
+            position: 'Board Member, Co-Chair',
+            company: 'Leadership Fond du Lac Alumni Board',
             companyDesc: '',
-            city: '',
-            state: '',
-            start: '',
-            end: '',
-            description: ''
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: 'July 2011',
+            end: 'August 2014',
+            description: '',
+            tech: [
+
+            ]
         },
         {
-            position: '',
-            company: '',
-            companyDesc: '',
-            city: '',
-            state: '',
-            start: '',
-            end: '',
-            description: ''
+            position: 'Steering Member, Web Developer, Emcee',
+            company: 'Cheers for Volunteers Recognition Event',
+            companyDesc: 'Fond du Lac',
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: '2002',
+            end: '2013',
+            description: '',
+            tech: [
+
+            ]
         },
         {
-            position: '',
-            company: '',
+            position: 'Developer',
+            company: 'BrownBoots Interactive',
+            companyDesc: 'Web development and interactive media marketing agency',
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: 'July 2005',
+            end: 'April 2012',
+            description: '•	Full Stack Developer on over 200 marketing websites for small to large organization using HTML4, CSS2, PHP, MySQL<br>•	Lead Technical Architect / Frontend Developer on CD-ROM and web-based international media press kits for Kohler Company and Mercury Marine using Flash, ActionScript, XML, HTML4, CSS2<br>• Improved profitability through the use of standardized processes and disciplined reporting<br>• Built data-driven model for project estimating that reduced project overruns<br>• Built and populated a web-based wiki which serves as company’s SOP and eliminates 50% of employee training effort<br>• Built and populated a web-based client training manual<br>• Developed international marketing multimedia for large organizations such as Kohler Company and Mercury Marine',
+            tech: [
+                'HTML/CSS',
+                'PHP',
+                'MySQL',
+                'XML',
+                'Flash/ActionScript',
+                'PhotoShop'
+            ]
+        },
+        {
+            position: 'Adjunct Professor',
+            company: 'Marian University of Fond du Lac',
             companyDesc: '',
-            city: '',
-            state: '',
-            start: '',
-            end: '',
-            description: ''
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: 'August 2011',
+            end: 'December 2011',
+            description: '• Taught critical thinking skills to freshman students<br>• Organized multimedia presentations<br>• Prepared class assignments and testing materials',
+            tech: [
+
+            ]
+        },
+        {
+            position: 'Project Manager',
+            company: 'SimpleC, LLC as Georgia Tech TI:GER Program student',
+            companyDesc: '',
+            city: 'Atlanta',
+            state: 'GA',
+            start: 'January 2011',
+            end: 'May 2011',
+            description: '• Prepared in-depth dementia and home healthcare industry analyses',
+            tech: [
+
+            ]
+        },
+        {
+            position: 'Student Manager and IT Support',
+            company: 'Georgia Institute of Technology IT Services',
+            companyDesc: '',
+            city: 'Atlanta',
+            state: 'GA',
+            start: 'August 2009',
+            end: 'May 2011',
+            description: '• Managed and provided end user support<br>• Reduced student employees staffing needs by 67%<br>• Wrote standard operating procedures for student training, workstation deployment, and other department processes<br>• Automated Windows 7 and computer deployment processes which reduced readiness time by 95%<br>• Trained and managed 15 student interns and graduate research assistants each semester',
+            tech: [
+                'Windows Server 2003, 2008',
+                'Windows XP, Vista, 7; OSX',
+                'Microsoft Deployment Toolkit'
+            ]
+        },
+        {
+            position: 'Project Manager',
+            company: 'MessageGears, LLC as Georgia Tech TI:GER Program student',
+            companyDesc: '',
+            city: 'Atlanta',
+            state: 'GA',
+            start: 'August 2010',
+            end: 'December 2010',
+            description: '• Conceptualized a novel social media monitoring and message delivery service',
+            tech: [
+
+            ]
+        },
+        {
+            position: 'Business Systems Analyst Intern',
+            company: 'Michels Corporation',
+            companyDesc: '',
+            city: 'Brownsville',
+            state: 'WI',
+            start: 'May 2010',
+            end: 'August 2010',
+            description: '• Assisted with SharePoint project management implementation to centralize the collection and management of project resources<br>• Documented operational workflows from initial bid to last payment; suggested improvements to executive management',
+            tech: [
+                'SharePoint',
+                'Prezi',
+                'Excel'
+            ]
+        },
+        {
+            position: 'Developer',
+            company: 'BrownBoots Interactive',
+            companyDesc: 'Web development and interactive media marketing agency',
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: 'July 2005',
+            end: 'July 2009',
+            description: '• Developed e-commerce solutions that earn first-page search rankings and increase access and sales to targeted markets<br>• Introduced clientele to technologies that enable them to manage web content and make informed marketing decisions<br>• Developed international marketing multimedia for large organizations such as Kohler Company and Mercury Marine<br>• Redeveloped content management system which improved user interface, training time, and application capabilities',
+            tech: [
+                'HTML/CSS',
+                'Flash/ActionScript',
+                'PhotoShop'
+            ]
+        },
+        {
+            position: 'Board Member, Secretary, Web Developer',
+            company: 'Habitat for Humanity of Fond du Lac County',
+            companyDesc: '',
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: '2008',
+            end: '2009',
+            description: '',
+            tech: [
+
+            ]
+        },
+        {
+            position: 'Communications Council Member',
+            company: 'Fond du Lac Association of Commerce',
+            companyDesc: '',
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: '2006',
+            end: '2009',
+            description: '',
+            tech: [
+
+            ]
+        },
+        {
+            position: 'Financial Representative',
+            company: 'Northwestern Mutual',
+            companyDesc: '',
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: '2007',
+            end: '2008',
+            description: '• Provided needs-based personal financial analysis and solutions to help clients achieve their long-term financial goals and objectives<br>• Series 6/63 certification',
+            tech: [
+
+            ]
+        },
+        {
+            position: 'Internet Manager',
+            company: 'Van Horn Hyundai of Fond du Lac',
+            companyDesc: '',
+            city: 'Fond du Lac',
+            state: 'WI',
+            start: 'April 2003',
+            end: 'December 2004',
+            description: '• Developed and managed Internet sales program<br>• Specified, set up and maintained cellular phones and computers<br>• Designed and constructed the dealership\'s first website',
+            tech: [
+                'HTML/CSS',
+                'AutoByTel',
+                'Autotrader',
+                'Cars.com',
+                'CarPoint',
+                'CarsDirect',
+                'eBay',
+                'GetAuto',
+                'InvoiceDealers',
+                'StoneAge'
+            ]
+        },
+        {
+            position: 'Network Administrator',
+            company: 'Principia College',
+            companyDesc: '',
+            city: 'Elsah',
+            state: 'IL',
+            start: 'December 2000',
+            end: 'March 2002',
+            description: '• Administered multiple Novell, Windows, and Solaris servers and physical network<br>• Earned Certified Novell Administrator status and managed all Novell user and e-mail accounts',
+            tech: [
+                'Novell',
+                'ZENworks',
+                'Solaris',
+                'Windows OSs',
+                'Mac OS',
+                'Norton Ghost',
+                'HEAT'
+            ]
+        },
+        {
+            position: 'Computer and Information Systems Technical Assistance Coordinator',
+            company: 'Principia College',
+            companyDesc: '',
+            city: 'Elsah',
+            state: 'IL',
+            start: 'June 1999',
+            end: 'December 2000',
+            description: '• Reduced students employees staffing needs by 50%<br>• Designed and automated PC deployment process which reduced readiness and services times by 75%<br>• Administered multiple Novell, Windows, and Solaris servers and physical network<br>• Supervised 20 part-time student technicians who maintained 450 school workstations',
+            tech: [
+                'Novell',
+                'ZENworks',
+                'Solaris',
+                'Windows OSs',
+                'Mac OS',
+                'Norton Ghost',
+                'HEAT'
+            ]
+        },
+        {
+            position: 'Lab Technician, MIS, Line Operator, Material Handler',
+            company: 'VPI, LLC',
+            companyDesc: '',
+            city: 'Sheboygan',
+            state: 'WI',
+            start: '1994',
+            end: '1999',
+            description: 'Summer and winter school break employment',
+            tech: [
+
+            ]
         }
     ];
 
