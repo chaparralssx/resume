a13191c546f9d285bd554817e54760df8eb6c502
R. Scott <rscott@theblameys.net>
2016-09-16 09:17:02 -0500

    Add education and volunteer experience to resume

--- src/app/client/default/controllers/resume.controller.js
+++ src/app/client/default/controllers/resume.controller.js
@@ -5,10 +5,12 @@ angular.module('app.default').controller('ResumeController', ResumeController);
 /**
  * Handles Resume page actions
  */
-function ResumeController($sce, contactData, summaryData, specialtiesData, experienceData) {
+function ResumeController($sce, contactData, summaryData, specialtiesData, experienceData, educationData, volunteerData) {
     var vm = this;
     vm.contact = contactData.data;
     vm.summary = summaryData.data;
     vm.specialties = specialtiesData.data;
     vm.experience = experienceData.data;
+    vm.education = educationData.data;
+    vm.volunteer = volunteerData.data;
 }
